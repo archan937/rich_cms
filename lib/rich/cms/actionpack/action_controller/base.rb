02ef777e2cdd619694fd3d9bdc4cb6b05f27deba
Paul Engel <paulengel@ArCHanGeL.local>
2010-08-15 12:50:29 +0200

    Moved rich/cms/controller.rb to rich/cms/actionpack/action_controller/base.rb

---
+++ lib/rich/cms/actionpack/action_controller/base.rb
@@ -0,0 +1,46 @@
+
+module ActionController
+  class Base
+
+    helper_method :current_rich_cms_admin, :current_rich_cms_admin_name, :rich_cms_authenticated_class, :rich_cms_authentication_inputs
+
+    def require_current_rich_cms_admin
+      unless current_rich_cms_admin
+        redirect_to root_url
+        return false
+      end
+    end
+
+    def current_rich_cms_admin
+      case rich_cms_auth.logic
+      when :authlogic
+        return @current_rich_cms_admin if defined?(@current_rich_cms_admin)
+        @current_rich_cms_admin_session ||= rich_cms_authenticated_class.find
+        @current_rich_cms_admin = @current_rich_cms_admin_session.try rich_cms_auth.specs[:class_name].demodulize.underscore
+      end
+    end
+
+    def current_rich_cms_admin_name
+      current_rich_cms_admin[rich_cms_auth.specs[:identifier]] if current_rich_cms_admin
+    end
+    
+    def rich_cms_auth
+      ::Rich::Cms::Engine.authentication
+    end
+    
+    def rich_cms_authenticated_class
+      case rich_cms_auth.logic
+      when :authlogic
+        "#{rich_cms_auth.specs[:class_name]}Session".constantize
+      end
+    end
+    
+    def rich_cms_authentication_inputs
+      case rich_cms_auth.logic
+      when :authlogic
+        rich_cms_auth.specs[:inputs] || [:email, :password]
+      end
+    end
+    
+  end
+end
