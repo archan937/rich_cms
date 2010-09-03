edf31694beaa9011079e7e564991189634e8c6ce
Paul Engel <paul.engel@holder.nl>
2010-09-03 13:11:03 +0200

    Made "/cms" a named route (called rich_cms)

--- lib/config/routes.rb
+++ lib/config/routes.rb
@@ -11,7 +11,7 @@ ActionController::Routing::Routes.draw do |map|
       rich.send "cms_#{action}", "cms/#{action}", :controller => "cms", :action => action
     end
     
-    rich.connect  "cms"         , :controller => "cms", :action => "display", :display => true
+    rich.cms      "cms"         , :controller => "cms", :action => "display", :display => true
     rich.cms_hide "cms/hide"    , :controller => "cms", :action => "display", :display => false
     rich.connect  "cms/position", :controller => "cms", :action => "position"
     
