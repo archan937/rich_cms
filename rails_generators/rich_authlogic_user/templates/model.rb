57c6bbf9ab2c9cb5a9da85182648feb61dde8cc4
Mark Mulder <markmulder@gmail.com>
2010-11-01 14:45:29 +0100

    Remove old generators

--- rails_generators/rich_authlogic_user/templates/model.rb
+++
@@ -1,8 +0,0 @@
-class <%= model_class_name %> < ActiveRecord::Base
-
-  acts_as_authentic do |c|
-    c.login_field            = :email
-    c.crypted_password_field = :crypted_password
-  end
-  
-end
--- rails_generators/rich_authlogic_user/templates/session.rb
+++
@@ -1,7 +0,0 @@
-class <%= model_class_name %>Session < Authlogic::Session::Base
-
-  authenticate_with <%= model_class_name %>
-  generalize_credentials_error_messages true
-  params_key "user_credentials"
-
-end
