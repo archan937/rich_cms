0b32b80603ce906330e3c1bab68661a63396efc7
Mark Mulder <markmulder@gmail.com>
2010-11-01 12:25:09 +0100

    Wip on changing lib

--- lib/rich/cms/engine.rb
+++ lib/rich/cms/engine.rb
@@ -1,32 +1,27 @@
 module Rich
   module Cms
-    module Engine
-
-      class RichCmsError < StandardError
-      end
-
-      extend self
+    class Engine < Rails::Engine
 
       attr_reader :authentication, :editable_content
 
-      def init
+      def self.init
         @authentication   = AuthenticationSpecs.new
         @editable_content = {}
 
-        %w(controllers).each do |dir|
-          path = File.join File.dirname(__FILE__), "..", "..", "app", dir
-          $LOAD_PATH << path
-          ActiveSupport::Dependencies.autoload_paths << path
-          ActiveSupport::Dependencies.autoload_once_paths.delete path
-        end
+          %w(controllers).each do |dir|
+            path = File.join File.dirname(__FILE__), "..", "..", "app", dir
+            $LOAD_PATH << path
+            ActiveSupport::Dependencies.autoload_paths << path
+            ActiveSupport::Dependencies.autoload_once_paths.delete path
+          end
 
-        ::Jzip::Engine.add_template_location({File.join(File.dirname(__FILE__), "..", "..", "assets", "jzip") => File.join(Rails.root, "public", "javascripts")})
-        ::Sass::Plugin.add_template_location( File.join(File.dirname(__FILE__), "..", "..", "assets", "sass"),   File.join(Rails.root, "public", "stylesheets") )
+          # ::Jzip::Engine.add_template_location({File.join(File.dirname(__FILE__), "..", "..", "assets", "jzip") => File.join(Rails.root, "public", "javascripts")})
+          # ::Sass::Plugin.add_template_location( File.join(File.dirname(__FILE__), "..", "..", "assets", "sass"),   File.join(Rails.root, "public", "stylesheets") )
 
-        copy_assets
+          # copy_assets
       end
 
-      def copy_assets
+      def self.copy_assets
         return if Rails.env == "test"
 
         source_dir = File.join File.dirname(__FILE__), "..", "..", "assets", "images", "."
@@ -37,16 +32,16 @@ module Rich
         FileUtils.cp_r    source_dir, target_dir
       end
 
-      def current_controller=(current_controller)
+      def self.current_controller=(current_controller)
         @current_controller  = current_controller
         @can_render_metadata = nil
       end
 
-      def authenticate(logic, specs)
+      def self.authenticate(logic, specs)
         @authentication = AuthenticationSpecs.new(logic, specs)
       end
 
-      def register(*args)
+      def self.register(*args)
         (editables = args.first.is_a?(Hash) ? args.first : Hash[*args]).each do |selector, specs|
           if @editable_content.keys.include?(selector)
             raise RichCmsError, "Already registered editable content identified with #{selector.inspect}"
@@ -56,15 +51,15 @@ module Rich
         end
       end
 
-      def to_content_tag(selector, identifiers, options = {})
+      def self.to_content_tag(selector, identifiers, options = {})
         editable_content[selector].fetch(identifiers).to_tag options
       end
 
-      def editable_content_javascript_hash
-        "{#{@editable_content.collect{|k, v| v.to_javascript_hash}.join ", "}}"
+      def self.editable_content_javascript_hash
+          "{#{@editable_content.collect{|k, v| v.to_javascript_hash}.join ", "}}"
       end
 
-      def can_render_metadata?
+      def self.can_render_metadata?
         if @can_render_metadata.nil?
           @can_render_metadata = case authentication.logic
                                  when :authlogic
@@ -77,12 +72,17 @@ module Rich
         end
       end
 
-    private
+      private
 
       AuthenticationSpecs = Struct.new(:logic, :specs)
 
     end
+    # class Rich::Cms::Engine < Rails::Engine
+
+    class RichCmsError < StandardError
+    end
+
   end
 end
 
-Rich::Cms::Engine.init
+# Rich::Cms::Engine.init
--- lib/rich_cms.rb
+++ lib/rich_cms.rb
@@ -1,12 +1,8 @@
-require "authlogic"
-require "jzip"
-require "sass/plugin"
+require 'authlogic'
+require 'jzip'
+# require 'sass/plugin'
 
-require "formtastic"
-ActionView::Base.send :include, Formtastic::SemanticFormHelper
+require 'formtastic'
 
-require "config/routes"
-require "rich/cms/actionpack"
 require "rich/cms/engine"
-require "rich/cms/content/group"
-require "rich/cms/content/item"
+
