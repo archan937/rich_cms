c78dd42615af32c52dfbe2a83097129e8cca10b8
Paul Engel <paulengel@archan937.local>
2010-11-03 01:47:34 +0100

    Made the routes Rails 2 and 3 compatible

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,36 @@
-Rails.application.routes.draw do
+if Rails::VERSION::MAJOR >= 3
 
-  # rich_cms_login  /cms/login                         {:controller=>"rich/cms", :action=>"login"}
-  scope :module => 'rich' do
-    %w(login logout update).each do |action|
-      match "/cms/#{action}" => "cms##{action}", :as => "rich_cms_#{action}"
+  Rails.application.routes.draw do
+    scope :module => "rich" do
+      %w(login logout update).each do |action|
+        match "/cms/#{action}" => "cms##{action}", :as => "rich_cms_#{action}"
+      end
+      match "/cms/"         => "cms#display", :as => "rich_cms"     , :display => true
+      match "/cms/hide"     => "cms#display", :as => "rich_cms_hide", :display => false
+      match "/cms/position" => "cms#position"
     end
-    match '/cms/' => 'cms#display', :as => 'rich_cms', :display => true
-    match '/cms/hide' => 'cms#display', :as => 'rich_cms_hide', :display => false
-    match '/cms/position' => 'cms#position', :as => 'rich_cms_position'
+  end
+
+else
+
+  # TODO: add routes the right way as this is evil
+  class << ActionController::Routing::Routes;self;end.class_eval do
+    define_method :clear!, lambda {}
+  end
+  # END
+
+  ActionController::Routing::Routes.draw do |map|
+    map.namespace :rich, :path_prefix => "" do |rich|
+
+      %w(login logout update).each do |action|
+        rich.send "cms_#{action}", "cms/#{action}", :controller => "cms", :action => action
+      end
+
+      rich.cms      "cms"         , :controller => "cms", :action => "display", :display => true
+      rich.cms_hide "cms/hide"    , :controller => "cms", :action => "display", :display => false
+      rich.connect  "cms/position", :controller => "cms", :action => "position"
+
+    end
   end
 
 end
-
