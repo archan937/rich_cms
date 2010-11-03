53e732d2b753618d76010c2d9991254cf367b82b
Paul Engel <paulengel@archan937.local>
2010-11-03 01:39:30 +0100

    Cleaned up lib/rich_cms.rb a bit

--- lib/rich_cms.rb
+++ lib/rich_cms.rb
@@ -1,12 +1,9 @@
-require 'authlogic'
-require 'jzip'
-# require 'sass/plugin'
+require "authlogic"
+require "formtastic"
+require "jzip"
+require "haml"
 
-require 'formtastic'
-
-require "rich/cms/engine"
 require "rich/cms/actionpack"
 require "rich/cms/engine"
 require "rich/cms/content/group"
 require "rich/cms/content/item"
-
