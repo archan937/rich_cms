4e0658ee3002b1e055bfd9d974f87bc3ab174ad7
Paul Engel <paulengel@archan937.local>
2010-11-03 01:43:35 +0100

    Providing the String class with html_safe when not already defined

---
+++ lib/rich/cms/core/string.rb
@@ -0,0 +1,6 @@
+
+require "rich/cms/core/string/html_safe"
+
+class String
+  include Rich::Cms::Core::String::HtmlSafe unless "".respond_to?(:html_safe)
+end
---
+++ lib/rich/cms/core/string/html_safe.rb
@@ -0,0 +1,16 @@
+
+module Rich
+  module Cms
+    module Core
+      module String
+        module HtmlSafe
+
+          def html_safe
+            self
+          end
+
+        end
+      end
+    end
+  end
+end
