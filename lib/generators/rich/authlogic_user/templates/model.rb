b50ed367afbf2501c80035bb8e6ffd56bd74a426
Paul Engel <paulengel@archan937.local>
2010-11-07 21:17:30 +0100

    Added the to_key method in the generator template to fix Authlogic in Rails 3

--- lib/generators/rich/authlogic_user/templates/model.rb
+++ lib/generators/rich/authlogic_user/templates/model.rb
@@ -5,4 +5,8 @@ class <%= model_class_name %> < ActiveRecord::Base
     c.crypted_password_field = :crypted_password
   end
 
+  def to_key
+    new_record? ? nil : [self.send self.class.primary_key]
+  end
+
 end
