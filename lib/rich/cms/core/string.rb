37ce312f467682f893df433db209955079ac0c2e
Paul Engel <paulengel@archan937.local>
2010-11-07 21:17:53 +0100

    Minor tweak in core/string.rb

--- lib/rich/cms/core/string.rb
+++ lib/rich/cms/core/string.rb
@@ -2,5 +2,5 @@
 require "rich/cms/core/string/html_safe"
 
 class String
-  include Rich::Cms::Core::String::HtmlSafe unless "".respond_to? :html_safe
+  include Rich::Cms::Core::String::HtmlSafe unless String.new.respond_to? :html_safe
 end
