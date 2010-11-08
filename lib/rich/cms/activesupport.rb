41c706d6ba219768e19927a8c9ec06452f0dd99a
Paul Engel <paul.engel@holder.nl>
2010-11-08 18:17:18 +0100

    Removed check on Rails major version for requiring active_support/dependencies

--- lib/rich/cms/activesupport.rb
+++ lib/rich/cms/activesupport.rb
@@ -1,3 +1,1 @@
-if Rails::VERSION::MAJOR < 3
-  require File.join(File.dirname(__FILE__), "activesupport", "active_support", "dependencies.rb")
-end
+require File.join(File.dirname(__FILE__), "activesupport", "active_support", "dependencies.rb")
