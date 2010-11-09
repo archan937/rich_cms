5dee612f1c8a483e95770aa05bed8557a214d9a0
Paul Engel <paulengel@archan937.local>
2010-11-09 01:36:39 +0100

    Defining the 'to_key' in the correct file (which fixes Authlogic in Rails 3)

--- lib/generators/rich/authlogic_user/templates/model.rb
+++ lib/generators/rich/authlogic_user/templates/model.rb
@@ -5,8 +5,4 @@ class <%= model_class_name %> < ActiveRecord::Base
     c.crypted_password_field = :crypted_password
   end
 
-  def to_key
-    new_record? ? nil : [self.send self.class.primary_key]
-  end
-
 end
--- lib/generators/rich/authlogic_user/templates/session.rb
+++ lib/generators/rich/authlogic_user/templates/session.rb
@@ -4,4 +4,8 @@ class <%= model_class_name %>Session < Authlogic::Session::Base
   generalize_credentials_error_messages true
   params_key "user_credentials"
 
+  def to_key
+    new_record? ? nil : [self.send self.class.primary_key]
+  end
+
 end
