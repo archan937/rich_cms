39113865226059ded11e8f76b0d60150a1e94bde
Paul Engel <paulengel@archan937.local>
2010-12-13 00:08:43 +0100

    Updated rich_cms.js of the dummy apps

--- test/rails-3/dummy/public/javascripts/rich_cms.js
+++ test/rails-3/dummy/public/javascripts/rich_cms.js
@@ -1,6 +1,47 @@
 
 var onRaccoonTipReady = function() {
 
+/* Implement indexOf ourselves as IE does not support it */
+if (!Array.prototype.indexOf)
+{
+  Array.prototype.indexOf = function(searchElement /*, fromIndex */)
+  {
+    "use strict";
+
+    if (this === void 0 || this === null)
+      throw new TypeError();
+
+    var t = Object(this);
+    var len = t.length >>> 0;
+    if (len === 0)
+      return -1;
+
+    var n = 0;
+    if (arguments.length > 0)
+    {
+      n = Number(arguments[1]);
+      if (n !== n)
+        n = 0;
+      else if (n !== 0 && n !== (1 / 0) && n !== -(1 / 0))
+        n = (n > 0 || -1) * Math.floor(Math.abs(n));
+    }
+
+    if (n >= len)
+      return -1;
+
+    var k = n >= 0
+          ? n
+          : Math.max(len - Math.abs(n), 0);
+
+    for (; k < len; k++)
+    {
+      if (k in t && t[k] === searchElement)
+        return k;
+    }
+    return -1;
+  };
+}
+
 $.ajaxFormHandlers = {};
 
 $.extend({
@@ -1384,10 +1425,10 @@ Rich.Cms.Editor = (function() {
 
     setTimeout(function() {
       if ($("#cleditor_input").length) {
-        $("#cleditor_input").cleditor({
+        $("#cleditor_input").data("cleditor", $("#cleditor_input").cleditor({
           width : 500,
           height: 300
-        })[0].focus();
+        })[0].focus());
       }
     }, 250);
   };
