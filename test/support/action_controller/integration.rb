085ee996cff0825f7646b30d50df659bd7bac309
Paul Engel <paulengel@archan937.local>
2010-12-13 00:14:02 +0100

    Being able to run a complete integration test with the Rails 2 and 3 dummy apps succesfully (woot!)

--- test/support/action_controller/integration.rb
+++ test/support/action_controller/integration.rb
@@ -7,12 +7,21 @@ module ActionController
 
     def login
       visit "/cms"
-      page.execute_script("$('div#rich_cms_dock a.login').click()")
-      within("fieldset.inputs") do
-        fill_in "Email"   , :with => "paul.engel@holder.nl"
-        fill_in "Password", :with => "testrichcms"
-      end
-      click_button "Login"
+      page.execute_script "$('div#rich_cms_dock a.login').click()"
+      fill_in_and_submit "#raccoon_tip", {:Email => "paul.engel@holder.nl", :Password => "testrichcms"}, "Login"
+    end
+
+    def logout
+      find("#rich_cms_dock").click_link "Logout"
+    end
+
+    def hide_dock
+      find("#rich_cms_dock").click_link "Hide"
+    end
+
+    def close_raccoontip
+      find("#raccoontip").click_link "Close"
+      assert !find("#raccoon_tip").visible?
     end
 
     def mark_content
@@ -21,6 +30,24 @@ module ActionController
 
     def edit_content(key)
       page.execute_script "$('.cms_content.marked[data-key=" + key + "]').click()"
+      assert find("#raccoon_tip").visible?
+    end
+
+    def fill_in_and_submit(selector, with, submit)
+      within "#{selector} fieldset.inputs" do
+        with.each do |key, value|
+          begin
+            fill_in key.to_s, :with => value
+          rescue Selenium::WebDriver::Error::ElementNotDisplayedError
+            page.execute_script "var input = $('#{selector} [name=\"#{key}\"]');" +
+                                "if (input.data('cleditor')) {" +
+                                "  input.val('#{value}');" +
+                                "  input.data('cleditor').updateFrame();" +
+                                "}"
+          end
+        end
+      end
+      find(selector).find_button(submit).click
     end
 
   end
