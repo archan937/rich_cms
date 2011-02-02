3afb6201326ed4e77c2cedfdeebd46ae0445ec77
Paul Engel <paul.engel@holder.nl>
2011-02-02 10:43:52 +0100

    Updated Rails generated source files within the Rails 3 dummy app

--- test/rails-3/dummy/config/application.rb
+++ test/rails-3/dummy/config/application.rb
@@ -1,12 +1,10 @@
 require File.expand_path('../boot', __FILE__)
 
-require "active_model/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_view/railtie"
-require "action_mailer/railtie"
+require 'rails/all'
 
-Bundler.require
+# If you have a Gemfile, require the gems listed there, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module Dummy
   class Application < Rails::Application
