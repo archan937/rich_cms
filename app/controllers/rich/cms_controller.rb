9f91c88fbb48ac5eed6f1ab0479ba0a294ade393
Paul Engel <paul.engel@holder.nl>
2011-02-03 13:10:07 +0100

    Moved the login and logout actions to Rich::CmsSessionsController which enables the Devise 1.0.x support

--- app/controllers/rich/cms_controller.rb
+++ app/controllers/rich/cms_controller.rb
@@ -1,31 +1,5 @@
 module Rich
   class CmsController < ::ApplicationController
-    
-    def login
-      Rich::Cms::Auth.login
-      if request.xhr?
-        render :update do |page|
-          if Rich::Cms::Auth.admin
-            page.reload
-          else
-            page["##{Rich::Cms::Auth.klass_symbol}_#{Rich::Cms::Auth.inputs.first}"].focus
-          end
-        end
-      else
-        redirect_to request.referrer
-      end
-    end
-    
-    def logout
-      Rich::Cms::Auth.logout
-      if request.xhr?
-        render :update do |page|
-          page.reload
-        end
-      else
-        redirect_to request.referrer
-      end 
-    end
 
     def display
       (session[:rich_cms] ||= {})[:display] = params[:display]
--- test/shared/tests/app/routing_test.rb
+++ test/shared/tests/app/routing_test.rb
@@ -3,13 +3,6 @@ require File.expand_path("../../../test_helper.rb", __FILE__)
 module App
   class RoutingTest < ActionController::TestCase
 
-    %w(login logout update).each do |action|
-      test "rich_#{action}" do
-        assert_routing     "/cms/#{action}", :controller => "rich/cms", :action => action
-        assert_named_route "/cms/#{action}", :"rich_cms_#{action}"
-      end
-    end
-
     test "rich_cms" do
       assert_routing     "/cms", :controller => "rich/cms", :action => "display", :display => true
       assert_named_route "/cms", :rich_cms
@@ -20,8 +13,18 @@ module App
       assert_named_route "/cms/hide", :rich_cms_hide
     end
 
-    test "cms/position" do
-      assert_routing "/cms/position", :controller => "rich/cms", :action => "position"
+    %w(login logout).each do |action|
+      test "rich_cms_#{action}" do
+        assert_routing     "/cms/#{action}", :controller => "rich/cms_sessions", :action => action
+        assert_named_route "/cms/#{action}", :"rich_cms_#{action}"
+      end
+    end
+
+    %w(position update).each do |action|
+      test "rich_cms_#{action}" do
+        assert_routing     "/cms/#{action}", :controller => "rich/cms", :action => action
+        assert_named_route "/cms/#{action}", :"rich_cms_#{action}"
+      end
     end
 
   end
