fd9a5544ca8a64c1d1841ee409eb8d5d3cea3079
Paul Engel <paul.engel@holder.nl>
2011-02-04 15:14:50 +0100

    Updated db/schema.rb to be compatible with the default generate Devise migration

--- test/shared/dummy/db/schema.rb
+++ test/shared/dummy/db/schema.rb
@@ -1,4 +1,4 @@
-ActiveRecord::Schema.define do
+ActiveRecord::Schema.define(:version => 19820801180828) do
 
   create_table "authlogic_users", :force => true do |t|
     t.string   "name"
@@ -19,20 +19,23 @@ ActiveRecord::Schema.define do
   add_index "authlogic_users", ["email"], :name => "index_authlogic_users_on_email"
 
   create_table "devise_users", :force => true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "encrypted_password"
-    t.string   "password_salt"
-    t.integer  "sign_in_count"
+    t.string   "email",                               :default => "", :null => false
+    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
+    t.string   "password_salt",                       :default => "", :null => false
+    t.string   "reset_password_token"
+    t.string   "remember_token"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                       :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.datetime "current_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.datetime "last_sign_in_at"
-    t.string   "last_sign_in_ip"
   end
 
   add_index "devise_users", ["email"], :name => "index_devise_users_on_email", :unique => true
+  add_index "devise_users", ["reset_password_token"], :name => "index_devise_users_on_reset_password_token", :unique => true
 
   create_table "cms_contents", :force => true do |t|
     t.string   "key"
