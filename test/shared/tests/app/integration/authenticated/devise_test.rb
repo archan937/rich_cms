ac8056a0a0ffb8aa2d265e5c15f20bcbb86b4f30
Paul Engel <paul.engel@holder.nl>
2011-02-06 04:56:39 +0100

    Replacing the Devise pepper when running devise_test.rb which fixed logging in during the integration test (note: all six integration tests are succeeding, woot!)

--- test/shared/tests/app/integration/authenticated/devise_test.rb
+++ test/shared/tests/app/integration/authenticated/devise_test.rb
@@ -3,6 +3,7 @@ require File.expand_path("../../../../dummy_app.rb", __FILE__)
 DummyApp.setup do |app|
   app.generate_cms_admin
   app.restore_admin_fixtures
+  app.replace_devise_pepper
   app.generate_cms_content
 end
 
--- test/shared/tests/dummy_app.rb
+++ test/shared/tests/dummy_app.rb
@@ -57,6 +57,18 @@ module DummyApp
     end
   end
 
+  def replace_devise_pepper
+    devise_config = expand_path("config/initializers/devise.rb")
+    lines         = File.open(devise_config).readlines
+    pepper        = "b57fdc3ba6288df70ca46cf2f7a6c168408e2fd602a185deda0340db3f1b4427d7c02e94880ec3786a26c248ff40b12f4e39d3315bb7ddf5541b9efb27ecd8f7"
+
+    File.open(devise_config, "w") do |file|
+      lines.each do |line|
+        file << line.gsub(/(config\.pepper = ").*(")/, "config.pepper = \"#{pepper}\"")
+      end
+    end
+  end
+
   def generate_cms_content
     klass = "CmsContent"
 
