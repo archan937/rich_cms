e3ceb63c2f7b9f1e4131c9b5ebe906610514bc4f
Paul Engel <paul.engel@holder.nl>
2011-02-09 23:36:46 +0100

    Trivial change within test_helper.rb

--- test/shared/tests/test_helper.rb
+++ test/shared/tests/test_helper.rb
@@ -12,8 +12,4 @@ Dir[File.expand_path("../support/**/*.rb", __FILE__)].each do |file|
   require file
 end
 
-# if Rails 3
-# require File.expand_path("../../../lib/rich_cms", __FILE__)
-# include Rich::Cms
-
 puts "\nRunning Rails #{Rails::VERSION::STRING}\n\n"
