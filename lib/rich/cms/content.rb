11f2947c15cf216fd15ea2c615b8769d1776ce56
Paul Engel <paul.engel@holder.nl>
2011-02-14 23:32:09 +0100

    Introduced Rich::Cms::Content.key_for(key) (suitable for implementing translations)

--- lib/rich/cms/content.rb
+++ lib/rich/cms/content.rb
@@ -9,7 +9,7 @@ module Rich
 
       module ClassMethods
 
-        delegate :[], :fetch, :[]=, :delete, :key?, :has_key?, :store, :update_key, :clear, :to => :cache
+        delegate :clear, :to => :cache
 
         def setup(engine, options = {}, &block)
           @specs = Specs.new
@@ -18,7 +18,24 @@ module Rich
           yield specs if block_given?
         end
 
+        def method_missing(method, *args)
+          if DELEGATED.include?(method)
+            key = args.shift
+            cache.send method, *args.unshift(key_for(key))
+          else
+            super
+          end
+        end
+
+      protected
+
+        def key_for(key)
+          key
+        end
+
       private
+
+        DELEGATED = [:[], :fetch, :[]=, :delete, :key?, :has_key?, :store, :update_key]
 
         delegate :cache, :to => :specs
 
--- test/shared/tests/unit/content_test.rb
+++ test/shared/tests/unit/content_test.rb
@@ -19,32 +19,69 @@ class ContentTest < ActiveSupport::TestCase
     end
 
     context "using on the memory cache engine" do
-      setup do
-        class Content
-          include Rich::Cms::Content
-          setup :memory
-        end
-        Content.clear
-      end
-
-      should "have Moneta::Memory as cache" do
-        assert Content.send(:cache).is_a?(Moneta::Memory)
-      end
-
-      should "be able to read / write values" do
-        key, value = :some_key, "foobar"
-        Content[key] = value
-        assert_equal Content[key], value
-      end
-
-      context "when having created an instance" do
+      context "as simple Rich-CMS content" do
         setup do
-          @content = Content.new
+          class Content
+            include Rich::Cms::Content
+            setup :memory
+          end
+          Content.clear
         end
 
-        should "respond to all excepted methods" do
-          assert @content.respond_to?(:default_value)
-          assert @content.respond_to?(:to_tag)
+        should "have Moneta::Memory as cache" do
+          assert Content.send(:cache).is_a?(Moneta::Memory)
+        end
+
+        should "be able to read / write values" do
+          key, value = :some_key, "foobar"
+          Content[key] = value
+          assert_equal value, Content[key]
+        end
+
+        context "when having created an instance" do
+          setup do
+            @content = Content.new
+          end
+
+          should "respond to all excepted methods" do
+            assert @content.respond_to?(:default_value)
+            assert @content.respond_to?(:to_tag)
+          end
+        end
+      end
+
+      context "as Rich-CMS content with an overriden key (e.g. Translation)" do
+        setup do
+          class I18n
+            def self.locale
+              :nl
+            end
+            def self.t(key)
+              {:world => "wereld"}[key.to_sym]
+            end
+          end
+          class Translation
+            include Rich::Cms::Content
+            setup :memory
+          protected
+            def self.key_for(key)
+              "#{I18n.locale}:#{key}"
+            end
+          end
+          Translation.clear
+          @key, @value = "hello", "hallo"
+        end
+
+        should "be able to override passed keys" do
+          Translation.send(:cache).expects(:[]).with("#{I18n.locale}:#{@key}")
+          Translation[@key]
+          Translation.send(:cache).expects(:[]=).with("#{I18n.locale}:#{@key}", @value)
+          Translation[@key] = @value
+        end
+
+        should "be able to read / write values" do
+          Translation[@key] = @value
+          assert_equal @value, Translation[@key]
         end
       end
     end
