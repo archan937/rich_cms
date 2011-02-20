2487f76d44130c69b6f011871ca199b3c85e16f2
Paul Engel <paul.engel@holder.nl>
2011-02-20 08:54:24 +0100

    Trivial tweak of lib/rich_cms.rb

--- lib/rich_cms.rb
+++ lib/rich_cms.rb
@@ -13,8 +13,8 @@ require "rich/cms/core"
 require "rich/cms/activesupport"
 require "rich/cms/actionpack"
 require "rich/cms/rails"
-require "rich/cms/auth"
 require "rich/cms/engine"
 require "rich/cms/content"
+require "rich/cms/auth"
 
 require File.expand_path("../../config/routes", __FILE__) if Rails::VERSION::MAJOR < 3
