6d4bb148b7a3d5c8c25056338ba1551b119addd6
Paul Engel <paul.engel@holder.nl>
2011-02-23 00:46:48 +0100

    Adjusted .cms_content to .rcms_content

--- test/shared/tests/integration/authenticated/authlogic.rb
+++ test/shared/tests/integration/authenticated/authlogic.rb
@@ -26,55 +26,55 @@ module Authenticated
       should "behave as expected" do
         visit "/"
         assert page.has_no_css? "div#rich_cms_dock"
-        assert page.has_no_css? ".cms_content"
+        assert page.has_no_css? ".rcms_content"
         assert_equal "header"   , find(".left h1" ).text
         assert_equal "paragraph", find(".left div").text
 
         visit "/cms"
 
         assert page.has_css? "div#rich_cms_dock"
-        assert page.has_no_css? ".cms_content"
+        assert page.has_no_css? ".rcms_content"
 
         visit "/cms/hide"
         assert page.has_no_css? "div#rich_cms_dock"
-        assert page.has_no_css? ".cms_content"
+        assert page.has_no_css? ".rcms_content"
         login
 
         assert page.has_css? "div#rich_cms_dock"
         assert page.has_content? "Mark content"
-        assert_equal "< header >"   , find(".left h1.cms_content" ).text
-        assert_equal "< paragraph >", find(".left div.cms_content").text
+        assert_equal "< header >"   , find(".left h1.rcms_content" ).text
+        assert_equal "< paragraph >", find(".left div.rcms_content").text
 
         mark_content
-        assert page.has_css? ".cms_content.marked"
+        assert page.has_css? ".rcms_content.marked"
 
         edit_content "header"
-        assert_equal ".cms_content", find("#raccoon_tip input[name='content_item[__selector__]']").value
-        assert_equal ""            , find("#raccoon_tip input[name='content_item[value]']"       ).value
+        assert_equal ".rcms_content", find("#raccoon_tip input[name='content_item[__selector__]']").value
+        assert_equal ""             , find("#raccoon_tip input[name='content_item[value]']"       ).value
 
         fill_in_and_submit "#raccoon_tip", {:"content_item[value]" => "Try out Rich-CMS!"}, "Save"
-        assert_equal "Try out Rich-CMS!", find(".left h1.cms_content" ).text
-        assert_equal "< paragraph >"    , find(".left div.cms_content").text
+        assert_equal "Try out Rich-CMS!", find(".left h1.rcms_content" ).text
+        assert_equal "< paragraph >"    , find(".left div.rcms_content").text
 
         edit_content "paragraph"
-        assert_equal ".cms_content", find("#raccoon_tip input[name='content_item[__selector__]']").value
-        assert_equal ""            , find("#raccoon_tip textarea[name='content_item[value]']"    ).value
+        assert_equal ".rcms_content", find("#raccoon_tip input[name='content_item[__selector__]']").value
+        assert_equal ""             , find("#raccoon_tip textarea[name='content_item[value]']"    ).value
 
         fill_in_and_submit "#raccoon_tip", {:"content_item[value]" => "<p>Lorem ipsum dolor sit amet.</p>"}, "Save"
-        assert_equal "Try out Rich-CMS!"          , find(".left h1.cms_content"   ).text
-        assert_equal "Lorem ipsum dolor sit amet.", find(".left div.cms_content p").text
+        assert_equal "Try out Rich-CMS!"          , find(".left h1.rcms_content"   ).text
+        assert_equal "Lorem ipsum dolor sit amet.", find(".left div.rcms_content p").text
 
         hide_dock
         assert page.has_no_css? "div#rich_cms_dock"
-        assert page.has_css? ".cms_content"
+        assert page.has_css? ".rcms_content"
 
         visit "/cms"
         assert page.has_css? "div#rich_cms_dock"
-        assert page.has_css? ".cms_content"
+        assert page.has_css? ".rcms_content"
 
         logout
         assert page.has_no_css? "div#rich_cms_dock"
-        assert page.has_no_css? ".cms_content"
+        assert page.has_no_css? ".rcms_content"
         assert_equal "Try out Rich-CMS!"          , find(".left h1"   ).text
         assert_equal "Lorem ipsum dolor sit amet.", find(".left div p").text
       end
--- test/shared/tests/integration/non_authenticated.rb
+++ test/shared/tests/integration/non_authenticated.rb
@@ -21,38 +21,38 @@ class NonAuthenticatedTest < ActionController::IntegrationTest
     should "behave as expected" do
       visit "/"
       assert page.has_no_css? "div#rich_cms_dock"
-      assert_equal "< header >"   , find(".left h1.cms_content" ).text
-      assert_equal "< paragraph >", find(".left div.cms_content").text
+      assert_equal "< header >"   , find(".left h1.rcms_content" ).text
+      assert_equal "< paragraph >", find(".left div.rcms_content").text
 
       visit "/cms"
       assert page.has_css? "div#rich_cms_dock"
       assert page.has_content? "Mark content"
 
       mark_content
-      assert page.has_css? ".cms_content.marked"
+      assert page.has_css? ".rcms_content.marked"
 
       visit "/cms"
       mark_content
 
       edit_content "header"
-      assert_equal ".cms_content", find("#raccoon_tip input[name='content_item[__selector__]']").value
-      assert_equal ""            , find("#raccoon_tip input[name='content_item[value]']"       ).value
+      assert_equal ".rcms_content", find("#raccoon_tip input[name='content_item[__selector__]']").value
+      assert_equal ""             , find("#raccoon_tip input[name='content_item[value]']"       ).value
 
       fill_in_and_submit "#raccoon_tip", {:"content_item[value]" => "Try out Rich-CMS!"}, "Save"
-      assert_equal "Try out Rich-CMS!", find(".left h1.cms_content" ).text
-      assert_equal "< paragraph >"    , find(".left div.cms_content").text
+      assert_equal "Try out Rich-CMS!", find(".left h1.rcms_content" ).text
+      assert_equal "< paragraph >"    , find(".left div.rcms_content").text
 
       edit_content "paragraph"
-      assert_equal ".cms_content", find("#raccoon_tip input[name='content_item[__selector__]']").value
-      assert_equal ""            , find("#raccoon_tip textarea[name='content_item[value]']"    ).value
+      assert_equal ".rcms_content", find("#raccoon_tip input[name='content_item[__selector__]']").value
+      assert_equal ""             , find("#raccoon_tip textarea[name='content_item[value]']"    ).value
 
       fill_in_and_submit "#raccoon_tip", {:"content_item[value]" => "<p>Lorem ipsum dolor sit amet.</p>"}, "Save"
-      assert_equal "Try out Rich-CMS!"          , find(".left h1.cms_content"   ).text
-      assert_equal "Lorem ipsum dolor sit amet.", find(".left div.cms_content p").text
+      assert_equal "Try out Rich-CMS!"          , find(".left h1.rcms_content"   ).text
+      assert_equal "Lorem ipsum dolor sit amet.", find(".left div.rcms_content p").text
 
       hide_dock
       assert page.has_no_css? "div#rich_cms_dock"
-      assert page.has_css? ".cms_content"
+      assert page.has_css? ".rcms_content"
     end
   end
 
