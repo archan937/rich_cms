d303202383de63798bb98dfff21ca4cdb2471ff0
Paul Engel <paul.engel@holder.nl>
2011-02-23 01:09:15 +0100

    Fixed bug within Auth::Adapter

--- lib/rich/cms/auth/adapter.rb
+++ lib/rich/cms/auth/adapter.rb
@@ -3,7 +3,8 @@ module Rich
     module Auth
 
       class Adapter
-        cattr_accessor :current_controller
+
+        attr_accessor :current_controller
 
         delegate :sign_out, :warden, :params, :session, :to => :current_controller
         delegate :klass, :klass_symbol, :current_admin_method, :to => :specs
@@ -23,6 +24,11 @@ module Rich
         def admin
           raise NotImplementedError
         end
+
+      protected
+
+        attr_accessor :specs
+
       end
 
     end
