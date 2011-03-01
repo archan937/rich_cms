55e203dbee115f5edaa8ede6af44c8fd300984af
Paul Engel <paul.engel@holder.nl>
2011-03-01 02:23:20 +0100

    Using the Rich::Support.after_initialize hook

--- lib/rich/cms/engine.rb
+++ lib/rich/cms/engine.rb
@@ -8,35 +8,14 @@ module Rich
       class << self
 
         def init
-          append_to_load_path
-
-          if Rails::VERSION::MAJOR >= 3
-            config.after_initialize do
-              after_initialize
-            end
-          else
-            Rails.configuration.after_initialize do
-              after_initialize
-            end
+          Rich::Support.after_initialize do
+            register_assets
+            copy_images
+            require_models
           end
         end
 
       private
-
-        def append_to_load_path
-          %w(controllers).each do |dir|
-            path = File.join File.dirname(__FILE__), "..", "..", "app", dir
-            $LOAD_PATH << path
-            ActiveSupport::Dependencies.autoload_paths << path
-            ActiveSupport::Dependencies.autoload_once_paths.delete path
-          end
-        end
-
-        def after_initialize
-          register_assets
-          copy_images
-          require_models
-        end
 
         def register_assets
           ::Jzip::Engine.add_template_location({File.expand_path("../../../../assets/jzip", __FILE__) => File.join(Rails.root, "public", "javascripts")})
