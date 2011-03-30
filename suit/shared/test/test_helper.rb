fa7db1dd59e160a71af47df2ea8e8a654dc83f3e
Paul Engel <paul.engel@holder.nl>
2011-03-31 00:07:55 +0200

    Being able to forge Rich-i18n Improved gem lib requirement within test_helper.rb (being able to print error when loading)

--- suit/shared/test/test_helper.rb
+++ suit/shared/test/test_helper.rb
@@ -10,6 +10,15 @@ require "mocha"
 
 begin
   require File.expand_path("../../../../lib/rich_cms", __FILE__)
-rescue LoadError
-  require File.expand_path("../../../../../lib/rich_cms", __FILE__)
+rescue LoadError => e
+  begin
+    require File.expand_path("../../../../../lib/rich_cms", __FILE__)
+  rescue LoadError
+    puts "ERROR: #{e.message}\n\n"
+  end
+end
+
+def forge_rich_i18n
+  require File.expand_path("../suit_application/rich/i18n_forgery.rb", __FILE__)
+  Translation.send(:content_store).clear
 end
