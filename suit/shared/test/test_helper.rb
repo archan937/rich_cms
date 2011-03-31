1bd9006ba93da5b7feeb63f3f21001fe946c7bd5
Paul Engel <paul.engel@holder.nl>
2011-03-31 21:44:47 +0200

    Using gem_suit/test_help within test_helper.rb

--- suit/shared/test/test_helper.rb
+++ suit/shared/test/test_helper.rb
@@ -1,24 +1,9 @@
 require "rubygems"
+require "gem_suit/test_help"
 
-begin
-  require "rails/all"
-rescue LoadError
-end
-
-require "shoulda"
-require "mocha"
-
-begin
-  require File.expand_path("../../../../lib/rich_cms", __FILE__)
-rescue LoadError => e
-  begin
-    require File.expand_path("../../../../../lib/rich_cms", __FILE__)
-  rescue LoadError
-    puts "ERROR: #{e.message}\n\n"
+class ActiveSupport::TestCase
+  def forge_rich_i18n
+    require File.expand_path("../suit_application/rich/i18n_forgery.rb", __FILE__)
+    Translation.send(:content_store).clear
   end
-end
-
-def forge_rich_i18n
-  require File.expand_path("../suit_application/rich/i18n_forgery.rb", __FILE__)
-  Translation.send(:content_store).clear
 end
