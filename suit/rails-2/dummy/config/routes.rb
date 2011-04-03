3ba8b1f87ff2c04521505b6980e13a2f0e7fadfc
Paul Engel <paul.engel@holder.nl>
2011-04-03 11:14:14 +0200

    Added devise_for :devise_users within the dummy app routes.rb files

--- suit/rails-2/dummy/config/routes.rb
+++ suit/rails-2/dummy/config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
+  map.devise_for :devise_users
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -17,7 +18,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # Sample resource route with sub-resources:
   #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
-  
+
   # Sample resource route with more complex sub-resources
   #   map.resources :products do |products|
   #     products.resources :comments
